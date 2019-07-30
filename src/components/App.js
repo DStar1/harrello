2a0aba9223b975057c25ab2b64106b4d203c7115
dstar1 <harrisonfsmith95@gmail.com>
2019-07-30 13:07:48 -0700

    dragging and dropping perssists within it's list! Some bug fixes by using to styled-components library.

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,33 @@ import TrelloList from './TrelloList';
 import TrelloActionButton from './TrelloActionButton';
 import { connect } from "react-redux";
 import { DragDropContext } from "react-beautiful-dnd";
+import { sort } from "../actions";
+import styled from "styled-components";
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 class App extends React.Component {
 
-  onDragEnd = () => {
-    // TODO: reordering logic
-  }
+  onDragEnd = result => {
+    const { destination, source, draggableId } = result;
+    
+    if (!destination) {
+      return;
+    }
+
+    this.props.dispatch(
+      sort(
+        source.droppableId,
+        destination.droppableId,
+        source.index,
+        destination.index,
+        draggableId
+      )
+    );
+  };
 
   render() {
 
@@ -18,23 +39,15 @@ class App extends React.Component {
       <DragDropContext onDragEnd={this.onDragEnd}>
       <div className="App">
         <h2>Harrello</h2>
-          <div style={styles.listsContainer}>
+          <ListContainer>
           { lists.map(list =>
             <TrelloList listID={list.id} key={list.id} title={list.title} cards={list.cards} />
           )}
           <TrelloActionButton list />
-        </div>
+        </ListContainer>>
       </div>
       </DragDropContext>
     );
-  }
-}
-
-const styles = {
-  listsContainer: {
-    display: "flex",
-    flexDirection: "row",
-    marginRight: 8
   }
 }
 
